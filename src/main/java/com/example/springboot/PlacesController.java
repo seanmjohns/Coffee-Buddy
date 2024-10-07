88c8f131282731d44d10ba8db76c37fb83bee2da
Marko Milenkovic <66339508+markoscode@users.noreply.github.com>
2024-10-07 11:04:11 -0400

    Updated table with new fields and sorting (#18)
    * updated README with clearer instructions
    
    * added new data fields to location table with appropriate sort functionality

--- src/main/java/com/example/springboot/PlacesController.java
+++ src/main/java/com/example/springboot/PlacesController.java
@@ -1,5 +1,8 @@
 package com.example.springboot;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -11,19 +14,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PlacesController {
 
     @Value("${google.api.key}")
-    private String googleApiKey; // Add your Google API Key to application.properties
+    private String googleApiKey;
 
     @GetMapping("/")
     public String findPlaces(@RequestParam(value = "latitude", required = false) String latitude,
                  @RequestParam(value = "longitude", required = false) String longitude,
-                 @RequestParam(value = "radius", required = false) String radius, Model model,
-                 @RequestParam(value = "locationTypes", required = false) String locationTypes) {
+                 @RequestParam(value = "radius", required = false) String radius,
+                 @RequestParam(value = "locationTypes", required = false) String[] locationTypes,
+                 Model model) {
 
         model.addAttribute("longitude", longitude);
         model.addAttribute("latitude", latitude);
         model.addAttribute("radius", radius);
         model.addAttribute("googleApiKey", googleApiKey);
-        model.addAttribute("locationTypes", locationTypes != null ? locationTypes : new ArrayList<String>());
+        
+        List<String> types = locationTypes != null ? Arrays.asList(locationTypes) : new ArrayList<>();
+        model.addAttribute("locationTypes", types);
+        
         return "places";
     }
 }
